fc29cf3bf6225285deb0d0ca596c72bdf2960f23
Viacheslav Loparev <vloparev@upteam.com>
2022-11-20 22:59:18 +0100

    timeout fix

--- rh-tests/pages/BasePage.ts
+++ rh-tests/pages/BasePage.ts
@@ -45,7 +45,7 @@ export abstract class BasePage {
             const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
             for (let i = 0; i < document.body.scrollHeight; i += this.isMobile ? 150 : 200) {
                 window.scrollTo(0, i);
-                await delay(this.isMobile ? 20 : 70);
+                await delay(this.isMobile ? 25 : 70);
             }
         });
     }
